be34fedf3cdfb254bda78cd65f975bb1faa69439
Gourab <gourab.hazra@unyscape.com>
2023-08-10 15:56:55 +0530

    Added demo gif

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
 
         # Initialize PyAudio
         p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+        stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=4000)
 
         stop_button = st.button("Stop Recognition")  # Create a stop button
 
