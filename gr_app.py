05d7db6bea9bfabaf6948d2fc12768019476b0cd
Gourab <gourab.hazra@unyscape.com>
2023-08-11 15:33:52 +0530

    remove hugginface model

--- gr_app.py
+++ gr_app.py
@@ -1,16 +1,8 @@
 import gradio as gr
 import time
-from transformers import pipeline
 import whisper
 
-# model_hug = pipeline("automatic-speech-recognition")
 model = whisper.load_model("base.en")
-
-# def transcribe(audio, state=""):
-#     time.sleep(0.2)
-#     text = model_hug(audio)["text"]
-#     state += text + " "
-#     return state, state
 
 def transcribe_whisper(audio, state=""):
     time.sleep(0.5)
